d636b245a018b77af0a45f1061fd0a0b4a5eb8ba
SHIVAM0001-FLEX <80771521+SHIVAM0001-FLEX@users.noreply.github.com>
2021-04-20 13:14:16 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,40 +1,32 @@
-const { MessageEmbed } = require("discord.js");
+onst { MessageEmbed } = require("discord.js");
 const { EMOJI_DONE , BOT_ID } = require('../config.json');
 
-// message.react(EMOJI_DONE);
-
 module.exports = {
-  name: "h",
-  aliases: ["help"],
+  name: "help",
+  aliases: ["h"],
   description: "Display all commands and descriptions",
-  execute(message) {
-    let commands = message.client.commands.array();
+  execute(message, args) {
+    if(!args[0]) {
+      let commands = message.client.commands.map((x) => '`' + x.name + '`').join(', ');
 
-    let helpEmbed = new MessageEmbed()
-      .setTitle(`${message.client.user.username} Help`)
-      .setDescription(`This are the all commands of <@${BOT_ID}> ! ${EMOJI_DONE}`)
-      
+      let helpEmbed = new MessageEmbed()
+      .setAuthor(`Command List`, `${message.client.user.avatarURL()}`)
+      .setColor("#00ffff")
+      .setDescription(`\`All Commands Of\` **${message.client.user.username}**\n\n${commands}`)
+      .addField(`**Links**`, `**[Support Server](https://discord.gg/2UTZvZS3ma)** • **[Invite Me](https://discord.com/oauth2/authorize?client_id=824574998101884960&scope=bot&permissions=3435585)** `)
+
+      message.channel.send(helpEmbed)
+    } else {
+      const command = message.client.commands.get(args.join(" ").toLowerCase()) || message.client.commands.find(x => x.aliases && x.aliases.includes(args.join(" ").toLowerCase()));
+
+      if (!command) return message.channel.send(`I did not find this command !`);
+
+      const advhelp = new MessageEmbed()
+      .setAuthor(`Help Panel`, `${message.client.user.avatarURL()}`)
       .setColor("RED")
+      .setDescription(`Name: ${command.name}\nDescription: ${command.description}\nAliases: ${command.aliases.length < 1 ? 'None' : command.aliases.join(', ')}`)
 
-      .setFooter(`Hey Want to make a bot like this then - https://github.com/NIRMAL1090/ProMusicBot`);
-      
-      
-    commands.forEach((cmd) => {
-      helpEmbed.addField(
-        `**${message.client.prefix}${cmd.name} ${cmd.aliases ? `(${cmd.aliases})` : ""}**`,
-        `${cmd.description}`,
-        true
-      );
-     
-
-
-    });
-
-   // helpEmbed.setTimestamp();
-
-    return message.channel.send(helpEmbed).catch(console.error);
+      message.channel.send(advhelp)
+    }
   }
-};
-
-
-console.log("help working")
+}
