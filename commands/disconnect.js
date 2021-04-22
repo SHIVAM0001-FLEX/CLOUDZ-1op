087216504ea49c208054935a535e341966fc5e67
SHIVAM0001-FLEX <80771521+SHIVAM0001-FLEX@users.noreply.github.com>
2021-04-22 11:42:28 +0530

    Update disconnect.js

--- commands/disconnect.js
+++ commands/disconnect.js
@@ -1,28 +1,4 @@
-
-const { EMOJI_DONE } = require('../config.json');
-
-module.exports = {
-    name: "disconnect",
-    aliases: ["dc", "disconnect", "nikal"],
-    description: "Leaves VC",
-    async execute(message, args) {
-        const { channel } = message.member.voice;
-    
-        const serverQueue = message.client.queue.get(message.guild.id);
-        if (!channel) return message.reply("You need to join a voice channel which i'm in - to disconnect me!").catch(console.error);
-        if (serverQueue && channel !== message.guild.me.voice.channel)
-          return message.reply(`You must be in the same channel as ${message.client.user}`).catch(console.error);
-
-            message.member.voice.channel.leave();
-            
-           
-            message.react(EMOJI_DONE);
-        }
-    }
-
-
-
-    console.log("DC / Disconnect working")const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 
 module.exports = {
@@ -35,17 +11,17 @@ module.exports = {
         const serverQueue = message.client.queue.get(message.guild.id);
 
         const dcneedvc = new MessageEmbed()        
-        .setDescription(":astroz_wrong: You need to join a voice channel which i'm in - to disconnect me!")
-        .setColor("YELLOW");
+        .setDescription("<a:wrong:833366149785845790> You need to join a voice channel which i'm in - to disconnect me!")
+        .setColor("#FF0000");
 
         const dcembed = new MessageEmbed()
-        .setDescription(":astroz_correct: 24/7 mode is now **Disable** in this server.")
-        .setColor("YELLOW")
+        .setDescription("<a:right:833265255886356510> 24/7 mode is now **Disable** in this server.")
+        .setColor("#FF0000")
 
         
         const dccembed = new MessageEmbed()
-        .setDescription(`You must be in the same channel as ${message.client.user}`)
-        .setColor("YELLOW")
+        .setDescription(`<a:wrong:833366149785845790> You must be in the same channel as ${message.client.user}`)
+        .setColor("#FFOOOO")
 
         if (!channel) return message.reply(dcneedvc).catch(console.error);
         if (serverQueue && channel !== message.guild.me.voice.channel)
